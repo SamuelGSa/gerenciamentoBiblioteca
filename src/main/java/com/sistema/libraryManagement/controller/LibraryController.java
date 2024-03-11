91a17819e2e5685f6c4eb4ac5fa97da95d08be85
SamuelGSa <sgmsa@icloud.com>
2024-03-11 14:35:23 -0300

    mapper feito

--- src/main/java/com/sistema/libraryManagement/controller/LibraryController.java
+++ src/main/java/com/sistema/libraryManagement/controller/LibraryController.java
@@ -1,7 +1,10 @@
 package com.sistema.libraryManagement.controller;
 
+import com.sistema.libraryManagement.dto.BookDTO;
+import com.sistema.libraryManagement.mapper.BookMapper;
 import com.sistema.libraryManagement.model.Book;
 import com.sistema.libraryManagement.repository.LibraryRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +16,13 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/library")
+@AllArgsConstructor
 public class LibraryController {
 
     @Autowired
     LibraryRepository libraryRepository;
+
+    private final BookMapper bookMapper;
 
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks(@RequestParam(required = false) String title) {
@@ -29,11 +35,13 @@ public class LibraryController {
     }
 
     @PostMapping
-    public ResponseEntity<Book> saveBook(@RequestBody Book livro) {
+    public ResponseEntity<Book> saveBook(@RequestBody BookDTO livro) {
 
-        libraryRepository.save(livro);
+        Book book = bookMapper.mapDTOToModel(livro);
 
-        return new ResponseEntity<>(livro,HttpStatus.CREATED);
+        libraryRepository.save(book);
+
+        return new ResponseEntity<>(book,HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/sistema/libraryManagement/mapper/BookMapper.java
+++ src/main/java/com/sistema/libraryManagement/mapper/BookMapper.java
@@ -3,17 +3,15 @@ package com.sistema.libraryManagement.mapper;
 import com.sistema.libraryManagement.dto.BookDTO;
 import com.sistema.libraryManagement.model.Book;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BookMapper {
 
     private static final ModelMapper modelMapper = new ModelMapper();
 
-    public static Book toEntity(BookDTO bookDTO) {
+    public Book mapDTOToModel(BookDTO bookDTO) {
         return modelMapper.map(bookDTO, Book.class);
-    }
-
-    public static BookDTO toDto(Book book) {
-        return modelMapper.map(book, BookDTO.class);
     }
 
 
