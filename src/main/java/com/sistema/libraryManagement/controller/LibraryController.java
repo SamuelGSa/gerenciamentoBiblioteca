f38ad096a04dc0d2dca0b0ea705cc737240d690e
SamuelGSa <sgmsa@icloud.com>
2024-03-11 16:27:19 -0300

    put adc - crud completo

--- src/main/java/com/sistema/libraryManagement/controller/LibraryController.java
+++ src/main/java/com/sistema/libraryManagement/controller/LibraryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
@@ -55,4 +56,32 @@ public class LibraryController {
         return new ResponseEntity<>(requestedBook,HttpStatus.OK);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Optional<Book>> deleteBookById(@PathVariable Integer id) {
+        try {
+            libraryRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        }catch (NoSuchElementException nsee){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable Integer id, @RequestBody BookDTO newBook) {
+
+        return libraryRepository.findById(id)
+                .map(book -> {
+                    book.setTitle(newBook.getTitle());
+                    book.setActor(newBook.getActor());
+                    book.setIsbn(newBook.getIsbn());
+                    book.setPublishingCompany(newBook.getPublishingCompany());
+                    book.setPublicationDate(newBook.getPublicationDate());
+                    book.setGender(newBook.getGender());
+                    book.setNumberOfPages(newBook.getNumberOfPages());
+                    book.setModel(newBook.getModel().name());
+                    Book bookUpdated = libraryRepository.save(book);
+                    return ResponseEntity.ok().body(bookUpdated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
